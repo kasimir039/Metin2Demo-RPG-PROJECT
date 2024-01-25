2a0a907146b02c18dfec8df3663b6a54b4612e89
kasimir039 <kasimir39100@gmail.com>
2024-01-26 01:26:37 +0300

    delete  std::vector<CharacterAbilities> Abilities

--- RPG_PROJECT/PlayerAbstraction.h
+++ RPG_PROJECT/PlayerAbstraction.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "Player.h"
-#include "CharacterAbilities.h"
 #include "vector"
 
 class PlayerAbstraction {
 protected:
     Player* player;
-    std::vector<CharacterAbilities> Abilities;
     virtual void Display() const = 0;
 
 public:
